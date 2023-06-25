d49017d6b14c3c9d247722a46a1072e51f43ad73
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-06-25 09:02:29 +0700

    chore: move db credentials to env

--- database/database.go
+++ database/database.go
@@ -4,17 +4,23 @@ import (
 	"BNMO/models"
 	"database/sql"
 	"fmt"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var (
-	DATABASE *gorm.DB
+	DB *gorm.DB
 )
 
 func Initialize() {
-	dsn := "root:admin@tcp(localhost:3310)/bnmo?charset=utf8mb4&parseTime=True&loc=Local"
+	host := os.Getenv("MARIADB_HOST")
+	port := os.Getenv("MARIADB_PORT")
+	user := os.Getenv("MARIADB_USER")
+	pass := os.Getenv("MARIADB_PASS")
+	db_name := os.Getenv("MARIADB_DB")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, pass, host, port, db_name)
 
 	// Connect to database using gorm
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -28,64 +34,63 @@ func Initialize() {
 		&models.Request{},
 		&models.Transfer{})
 
-	DATABASE = db
-	seed(DATABASE)
+	DB = db
+	seed(DB)
 }
 
 func seed(db *gorm.DB) {
 	var accounts []models.Account
 	admin := models.Account{
-		IsAdmin: sql.NullBool{Bool: true, Valid: true},
+		IsAdmin:       sql.NullBool{Bool: true, Valid: true},
 		AccountStatus: sql.NullString{String: "accepted", Valid: true},
-		FirstName: "Admin",
-		LastName: "Admin",
-		Email: "admin@gmail.com", 
-		Username: "admin",
-		ImagePath: "./images/Admin.png",
+		FirstName:     "Admin",
+		LastName:      "Admin",
+		Email:         "admin@gmail.com",
+		Username:      "admin",
+		ImagePath:     "./images/Admin.png",
 		AccountNumber: "1",
-		Balance: 0,
+		Balance:       0,
 	}
 	admin.SetPassword("admin")
 
 	user1 := models.Account{
-		IsAdmin: sql.NullBool{Bool: false, Valid: true},
+		IsAdmin:       sql.NullBool{Bool: false, Valid: true},
 		AccountStatus: sql.NullString{String: "accepted", Valid: true},
-		FirstName: "John",
-		LastName: "Doe",
-		Email: "johndoe@gmail.com", 
-		Username: "johndoe123",
-		ImagePath: "./images/johndoe.png",
+		FirstName:     "John",
+		LastName:      "Doe",
+		Email:         "johndoe@gmail.com",
+		Username:      "johndoe123",
+		ImagePath:     "./images/johndoe.png",
 		AccountNumber: "100-100-1000",
-		Balance: 0,
+		Balance:       0,
 	}
 	user1.SetPassword("user1")
 
 	user2 := models.Account{
-		IsAdmin: sql.NullBool{Bool: false, Valid: true},
+		IsAdmin:       sql.NullBool{Bool: false, Valid: true},
 		AccountStatus: sql.NullString{String: "accepted", Valid: true},
-		FirstName: "Sarah",
-		LastName: "Baker",
-		Email: "sarahbaker@gmail.com", 
-		Username: "sarahbaker123",
-		ImagePath: "./images/sarahbaker.png",
+		FirstName:     "Sarah",
+		LastName:      "Baker",
+		Email:         "sarahbaker@gmail.com",
+		Username:      "sarahbaker123",
+		ImagePath:     "./images/sarahbaker.png",
 		AccountNumber: "100-100-1001",
-		Balance: 0,
+		Balance:       0,
 	}
 	user2.SetPassword("user2")
 
 	user3 := models.Account{
-		IsAdmin: sql.NullBool{Bool: false, Valid: true},
+		IsAdmin:       sql.NullBool{Bool: false, Valid: true},
 		AccountStatus: sql.NullString{String: "accepted", Valid: true},
-		FirstName: "Sam",
-		LastName: "Smith",
-		Email: "samsmith@gmail.com", 
-		Username: "samsmith123",
-		ImagePath: "./images/samsmith.png",
+		FirstName:     "Sam",
+		LastName:      "Smith",
+		Email:         "samsmith@gmail.com",
+		Username:      "samsmith123",
+		ImagePath:     "./images/samsmith.png",
 		AccountNumber: "100-100-1002",
-		Balance: 0,
+		Balance:       0,
 	}
 	user3.SetPassword("user3")
-
 
 	accounts = append(accounts, admin, user1, user2, user3)
 
