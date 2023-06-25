8dac9995e3de43a79d734c746ee2e0a1239c719b
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-06-25 09:29:42 +0700

    refactor: gorm models

--- gorm_models/customer.go
+++ gorm_models/customer.go
@@ -1,17 +1,21 @@
 package gormmodels
 
-import "github.com/google/uuid"
+import (
+	"BNMO/enum"
+
+	"github.com/google/uuid"
+)
 
 type Customer struct {
 	Base
-	Pin                string  `gorm:"unique; not_null"`
-	CardNumber         string  `json:"card_number" gorm:"unique; not_null"`
-	Balance            float32 `json:"balance" gorm:"not_null; default:0" sql:"type:decimal(12, 2)"`
-	Status             string  `json:"account_status" gorm:"not_null; default:'PENDING'"`
-	PhoneNumber        string  `json:"phone_number" gorm:"not_null"`
-	IdCardPath         string  `json:"id_card_path" gorm:"not_null"`
-	ProfilePicturePath string  `json:"profile_pic_path"`
-	Account            Account `json:"account"`
+	Pin                string             `gorm:"unique"`
+	CardNumber         string             `json:"card_number" gorm:"unique"`
+	Balance            float32            `json:"balance" gorm:"not_null; default:0" sql:"type:decimal(12, 2)"`
+	Status             enum.AccountStatus `json:"account_status" gorm:"not_null; default:'PENDING'"`
+	PhoneNumber        string             `json:"phone_number" gorm:"not_null"`
+	IdCardPath         string             `json:"id_card_path" gorm:"not_null"`
+	ProfilePicturePath string             `json:"profile_pic_path"`
+	Account            Account            `json:"account"`
 	AccountID          uuid.UUID
 	Associates         []*Customer `gorm:"many2many:customer_associates"`
 }
