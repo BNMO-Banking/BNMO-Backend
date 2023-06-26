849c08cc3a7b3c09eb6204da96e29e2f5e0a6d41
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-06-26 13:53:59 +0700

    feat: new env loader and router

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-redis/redis/v9 v9.0.0-beta.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.3.0
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.3.0
 	gorm.io/driver/mysql v1.3.5
 	gorm.io/gorm v1.23.8
--- main.go
+++ main.go
@@ -3,38 +3,40 @@ package main
 import (
 	"BNMO/database"
 	"BNMO/routes"
+	"log"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 var (
-	Router = gin.Default()
+	router = gin.Default()
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading env file", err.Error())
+	}
+
+	log.Println("Env successfully loaded")
+
 	// Initialize database using GORM
 	database.Initialize()
 
 	// Set up CORS policy
-	Router.Use(cors.New(cors.Config{
+	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "Static", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
-		MaxAge: 12 * time.Hour,
-	  }))
-	routes.MapUrls(Router)
-	Router.Run()
+		MaxAge:           12 * time.Hour,
+	}))
+
+	routes.AuthRoutes(router)
+	routes.ProfileRoutes(router)
+	router.Run()
 }
-
-/* func main() {
-	//var symbols exchange.SymbolStruct = exchange.GetSymbols()
-
-	//fmt.Println(symbols.Symbols)
-	for k, v := range symbols.Symbols.(map[string]interface{}) {
-		fmt.Println(k, "-", v)
-	}
-} */
